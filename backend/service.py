729931b81ea92af9ab54194b826f5b10743fdc66
Collin Brohm <152938275+collinbrohm@users.noreply.github.com>
2025-04-22 15:12:47 -0400

    added more routes

--- backend/service.py
+++ backend/service.py
@@ -1,32 +1,63 @@
 """Module that uses flask to middle-man between frontend and backend."""
 
+import logging
+
 from flask import  Flask, request, jsonify
 from flask_restful import Api, Resource
 
-# The post methods are to send to the datbase
-# The get methods are to retrieve from the database and display on the frontend.
+from database.client import DatabaseClient
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class User(Resource):
     def post():
-        pass
+        user_data = request.json
+        db = DatabaseClient()
+
+        if not user_data:
+            logger.error('No user data provided.')
+            return []
+        
+        user = db.transform_users(user_data)
+        db.persist_users(user)
 
     def get():
+        """Function do display user data."""
         pass
 
 class Team(Resource):
     def post():
-        pass
+        team_data = request.json
+        db = DatabaseClient()
+
+        if not team_data:
+            logger.error('No team data provided.')
+            return []
+        
+        team = db.transform_teams(team_data)
+        db.persist_teams(team)
 
     def get():
         pass
 
+class Player(Resource):
+    def post():
+        player_data = request.json
+        db = DatabaseClient()
+        
+        if not player_data:
+            logger.error('No player info provided.')
+            return[]
+        
+        player = db.transform_players(player_data)
+        db.persist_players(player)
 class Game(Resource):
     def post():
         pass
     
     def get():
         pass
-
-
 
 def main():
     app = Flask(__name__)
@@ -36,3 +67,5 @@ def main():
     api.add_resource(Game, '/game')
     app.run(host='127.0.0.1')
 
+if __name__ == '__main__':
+    main()
